ade196018f7ff29909e68556101a9ef2a38d1e7d
Stéphane Francel <contact@avine.io>
2017-03-15 15:34:19 +0100

    Add Less support (in progress)

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
-var webpack = require('webpack');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-var path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const extractLess = new ExtractTextPlugin({
+    filename: "[name].[contenthash].css",
+    disable: process.env.NODE_ENV === "development"
+});
 
 module.exports = {
 
   entry: {
     bundle: './src/js/app.js',
+    style: './src/less/app.less',
     vendor: 'jquery'
   },
 
@@ -15,22 +22,33 @@ module.exports = {
   },
 
   module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader'
-      }
-    ]
+    loaders: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader'
+    }, {
+      test: /\.less$/,
+      exclude: /(node_modules|bower_components)/,
+      //loader: ['css-loader', 'less-loader'],
+      loader: extractLess.extract({
+        use: [{
+          loader: "css-loader"
+        }, {
+          loader: "less-loader"
+        }]
+      })
+    }]
+
   },
 
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
       name: ['vendor'/*, 'manifest'*/]
     }),
-    new CopyWebpackPlugin([
-      { from: 'src/index.html', to: 'index.html' }
-    ])
+    new CopyWebpackPlugin([{
+      from: 'src/index.html', to: 'index.html'
+    }]),
+    extractLess
   ],
 
   devtool: 'source-map',
